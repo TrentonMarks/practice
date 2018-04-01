30ca8486e95147e9dd8de88537e5bd2521f889b4
Trenton Marks <trentmarks111@gmail.com>
2018-04-01 15:20:02 -0600

    the create-favorite form successfully sends the submitted data into the db via the createFavorite method, the index.html was updated to match the method name

--- favorites/public/js/favorites.js
+++ favorites/public/js/favorites.js
@@ -2,35 +2,46 @@ const app = angular.module('favorites', []);
 
 app.controller('MainController', ['$http', function($http){
     this.favorites = [];
-    this.createForm = {};
 
     // gets all favorites in the database
-    this.getFavorites = ()=>{
-        $http({
-            method:'GET',
-            url:'/favorites'
-        }).then(response=>{
-            this.favorites = response.data;
-        });
-    };
+    // this.getFavorites = ()=>{
+    //     $http({
+    //         method:'GET',
+    //         url:'/favorites'
+    //     }).then(response =>{
+    //         this.favorites = response.data;
+    //     }, error =>{
+    //         console.log(error);
+    //     });
+    // };
 
     // creates a new favorite
     this.createFavorite = ()=>{
         $http({
             method:'POST',
             url:'/favorites',
-            data: this.createForm
-        }).then(response=>{
+            data: {
+                source: this.source,
+                url: this.url
+            }
+        }).then(response =>{
             console.log(response.data);
             // this.createForm = {} removes text from form once submit
             this.createForm = {};
             this.favorites.push(response.data);
-        }, ()=>{
-            console.log('error');
+        }, error =>{
+            console.log(error);
         });
     };
 
+
+
+
+
+
+
+
     // loads immediately on page load
-    this.getFavorites();
+    // this.getFavorites();
 
 }]);
